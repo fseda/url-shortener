784a24b149a347c7b79f32419cd24e6f697999ad
fseda <felipeseda@poli.ufrj.br>
2023-08-08 23:36:29 -0300

    Refactor main file

--- main.go
+++ main.go
@@ -1,55 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"log"
 	"net/http"
-	"strings"
+	Handlers "github.com/fseda/url-shortener/src/handlers"
 )
-
-var (
-	tmplPath  = "./static/templates/"
-	templates = template.Must(template.ParseFiles(tmplPath + "index.html"))
-)
-
-type Url struct {
-	ID           int64  `json:"id"`
-	ShortenedUrl string `json:"shortened_url"`
-	OriginalUrl  string `json:"original_url"`
-}
 
 func main() {
-	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/", Handlers.IndexHandler)
+	http.HandleFunc("/view/", Handlers.ViewHandler)
+
 	http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir("static/styles/"))))
 	http.Handle("/scripts/", http.StripPrefix("/scripts/", http.FileServer(http.Dir("static/scripts/"))))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	url := r.FormValue("url")
-
-	var shortUrl string;
-	if url == "" {
-		shortUrl = ""
-	} else {
-		shortUrl = "bit.ly/" + strings.TrimPrefix(url, "https://")
-	}
-
-	newUrl := Url{
-		ID: 1,
-		ShortenedUrl: shortUrl,
-		OriginalUrl: url,
-	}
-
-	renderTemplate(w, "index", newUrl)
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, data Url) {
-	fmt.Println(data)
-	err := templates.ExecuteTemplate(w, tmpl+".html", data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
 }
