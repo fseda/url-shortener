99483ee25649a6f36e89df8969dce868044a2d18
fseda <felipeseda@poli.ufrj.br>
2023-08-08 23:33:37 -0300

    Change implementation of event listener

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,17 +1,13 @@
-const copyButtons = document.querySelectorAll('.copy-button');
-copyButtons.forEach(copyButton => {
-    copyButton.addEventListener('click', () => {
-        const shortenedUrl = copyButton.closest('.result').querySelector('.shortened-url a');
-        if (shortenedUrl) {
-            const tempInput = document.createElement('input');
-            tempInput.value = shortenedUrl.innerHTML;
-            document.body.appendChild(tempInput);
-            tempInput.select();
-            document.execCommand('copy');
-            document.body.removeChild(tempInput);
-            copyButton.querySelector('.copy-icon').textContent = '✅';
-        }
-    });
+const copyButton = document.querySelector('.copy-button');
+copyButton.addEventListener('click', () => {
+  const shortenedUrl = copyButton.closest('.result').querySelector('.shortened-url a');
+  if (shortenedUrl) {
+    const tempInput = document.createElement('input');
+    tempInput.value = shortenedUrl.innerHTML;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+    copyButton.querySelector('.copy-icon').textContent = '✅';
+    }
 });
-
-localStorage.removeItem("shortenedUrl");
