406eda950acb97f3f3b274558e61b7c31538cc5e
fseda <felipeseda@poli.ufrj.br>
2023-08-14 10:03:57 -0300

    Create DB variable

--- src/config/config.go
+++ src/config/config.go
@@ -3,11 +3,12 @@ package config
 import (
 	"database/sql"
 	// "fmt"
-	"os"
 
 	"github.com/joho/godotenv"
 	_ "github.com/mattn/go-sqlite3"
 )
+
+var DB *sql.DB
 
 func Config(key string) (string, error) {
 	err := godotenv.Load(".env")
@@ -19,44 +20,14 @@ func Config(key string) (string, error) {
 }
 
 func InitializeDB() {
-	dbPath := os.Getenv("DB_PATH")
-	// tableName := "urls"
+	var err error
+	dbPath, _ := Config("DB_PATH")
 
-	// _, err := os.Stat(dbPath)
-	// if err == nil {
-	// 	// File exists
-
-	// 	// Open connection
-	// 	db, err := sql.Open("sqlite3", "db.sqlite")
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	defer db.Close()
-
-	// 	createTableSQL := `
-	// 	CREATE TABLE IF NOT EXISTS urls (
-	// 		id INTEGER PRIMARY KEY AUTOINCREMENT,
-	// 		url TEXT
-	// 	)
-	// 	`
-
-	// 	_, err = db.Exec(createTableSQL)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	defer db.Close()
-
-	// } else if os.IsNotExist(err) {
-	// 	fmt.Println("File does not exist")
-	// } else {
-	// 	fmt.Println("Error:", err)
-	// }
-
-	db, err := sql.Open("sqlite3", dbPath)
+	DB, err = sql.Open("sqlite3", dbPath)
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
+	defer DB.Close()
 
 	createTableSQL := `
 	CREATE TABLE IF NOT EXISTS urls (
@@ -65,10 +36,9 @@ func InitializeDB() {
 	)
 	`
 
-	_, err = db.Exec(createTableSQL)
+	_, err = DB.Exec(createTableSQL)
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
-
+	defer DB.Close()
 }
