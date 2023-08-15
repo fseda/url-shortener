0c3d391298cf3df303005fbb812c20c0abdc3537
fseda <felipeseda@poli.ufrj.br>
2023-08-15 14:42:31 -0300

    Refactor IndexHandler to redirect to another handler

--- src/handlers/handlers.go
+++ src/handlers/handlers.go
@@ -13,30 +13,27 @@ import (
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	us := services.NewUrlService()
 
+	url := r.FormValue("url")
 	id := r.URL.Query().Get("id")
 	if id == "" {
-		url := r.FormValue("url")
 		if url == "" {
 			templates.RenderTemplate(w, "index", models.Url{})
 			return
 		}
 
-		baseUrl := getBaseURL(r)
-		shortenedUrl, err := us.ShortenUrl(url, baseUrl)
-		if err != nil {
-			ErrorHandler(w, r)
-		}
-
-		templates.RenderTemplate(w, "index", models.Url{ShortenedUrl: shortenedUrl})
+		// Show shortened url
+		redirectString := fmt.Sprintf("/view?url=%s", url)
+		http.Redirect(w, r, redirectString, http.StatusSeeOther)
 		return
-	}
-
+	} 
+	
 	if !isInt(&id) {
 		fmt.Println("id is not int")
 		ErrorHandler(w, r)
 		return
 	}
 
+	// Look for url with given id
 	intId, _ := strconv.Atoi(id)
 	url, err := us.GetUrl(intId)
 	if err != nil {
@@ -45,7 +42,8 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, url, http.StatusSeeOther)
+	// url is found and app redirects to it
+	http.Redirect(w, r, url, http.StatusFound)
 }
 
 func ErrorHandler(w http.ResponseWriter, r *http.Request) {
@@ -53,7 +51,23 @@ func ErrorHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func ViewHandler(w http.ResponseWriter, r *http.Request) {
-	templates.RenderTemplate(w, "view", models.Url{})
+	us := services.NewUrlService()
+
+
+	url := r.URL.Query().Get("url")
+	if url == "" {
+		http.Redirect(w, r, "/", http.StatusBadRequest)
+	}
+	fmt.Println("url", url)
+	baseUrl := getBaseURL(r)
+	shortenedUrl, err := us.ShortenUrl(url, baseUrl)
+	if err != nil {
+		ErrorHandler(w, r)
+	}
+
+	fmt.Println(shortenedUrl)
+
+	templates.RenderTemplate(w, "view", models.Url{ShortenedUrl: shortenedUrl})
 }
 
 func MakeHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
