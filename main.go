31fd4375f54c4ebc16dac98d027fd7539b6e41c1
fseda <felipeseda@poli.ufrj.br>
2023-08-15 11:44:05 -0300

    Create wrapper handlers

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	Handlers "github.com/fseda/url-shortener/src/handlers"
-	Config "github.com/fseda/url-shortener/src/config"
+
+	"github.com/fseda/url-shortener/src/config"
+	"github.com/fseda/url-shortener/src/handlers"
 )
 
 func main() {
-	Config.InitializeDB()
+	err := config.InitializeDB()
+	if err != nil {
+		panic(err)
+	}
+	defer config.DB.Close()
 
-	http.HandleFunc("/", Handlers.IndexHandler)
-	http.HandleFunc("/view/", Handlers.ViewHandler)
+	http.HandleFunc("/", handlers.MakeHandler(handlers.IndexHandler))
+	http.HandleFunc("/error", handlers.MakeHandler(handlers.ErrorHandler))
+	// http.HandleFunc("/view/", Handlers.ViewHandler)
 
 	http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir("static/styles/"))))
 	http.Handle("/scripts/", http.StripPrefix("/scripts/", http.FileServer(http.Dir("static/scripts/"))))
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	port, _ := config.Config("PORT")
+
+	fmt.Println("Listening on port " + port + "... 🔥")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
