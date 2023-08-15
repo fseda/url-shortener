c516cf8aa67e4a64b42adb12c1312a43a04f27a6
fseda <felipeseda@poli.ufrj.br>
2023-08-15 12:10:34 -0300

    Simplify template rendering

--- src/templates/templates.go
+++ src/templates/templates.go
@@ -3,49 +3,21 @@ package templates
 import (
 	"html/template"
 	"net/http"
-	"sync"
+	"regexp"
 
 	Models "github.com/fseda/url-shortener/src/models"
 )
 
 var (
-	tmplPath           = "./static/templates/"
-	templates          = template.Must(template.ParseGlob(tmplPath + "*.html"))
-	templateCacheMutex sync.Mutex
-	templateCache      = make(map[string]*template.Template)
+	tmplPath  = "./static/templates/"
+	templates = template.Must(template.ParseFiles(tmplPath+"index.html", tmplPath+"error.html"))
+	ValidPath = regexp.MustCompile("^/(|error)/([a-zA-Z0-9]+)$")
 )
 
-func getCachedTemplate(templateName string) (*template.Template, error) {
-	templateCacheMutex.Lock()
-	defer templateCacheMutex.Unlock()
-
-	tmpl, found := templateCache[templateName]
-	if !found {
-		tmpl, err := templates.Clone()
-		if err != nil {
-			return nil, err
-		}
-
-		tmpl, err = tmpl.ParseFiles(tmplPath + templateName + ".html")
-		if err != nil {
-			return nil, err
-		}
-
-		templateCache[templateName] = tmpl
-	}
-
-	return tmpl, nil
-}
-
 func RenderTemplate(w http.ResponseWriter, tmplName string, data Models.Url) {
-	tmpl, err := getCachedTemplate(tmplName)
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	err = tmpl.ExecuteTemplate(w, tmplName+".html", data)
+	err := templates.ExecuteTemplate(w, tmplName+".html", data)
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
+
