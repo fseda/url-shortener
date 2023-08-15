5601db2b9e61eb7845528e0585c9b456766171aa
fseda <felipeseda@poli.ufrj.br>
2023-08-15 10:10:49 -0300

    Add error handling

--- src/handlers/handlers.go
+++ src/handlers/handlers.go
@@ -1,17 +1,51 @@
 package handlers
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
-	Templates "github.com/fseda/url-shortener/src/templates"
-	Models "github.com/fseda/url-shortener/src/models"
+	"strconv"
+
+	custom_errors "github.com/fseda/url-shortener/src/errors"
+	"github.com/fseda/url-shortener/src/models"
+	"github.com/fseda/url-shortener/src/services"
+	"github.com/fseda/url-shortener/src/templates"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	Templates.RenderTemplate(w, "index", Models.Url{})
+	us := services.NewUrlService()
+
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		fmt.Println("id == \"\"")
+		templates.RenderTemplate(w, "index", models.Url{})
+		// handleError(w, custom_errors.ErrBadRequest)
+		return
+	}
+
+	if !isInt(&id) {
+		fmt.Println("id is not int")
+		handleError(w, custom_errors.ErrBadRequest)
+		return
+	}
+
+	intId, _ := strconv.Atoi(id)
+	url, err := us.GetUrl(intId)
+	if err != nil {
+		fmt.Println("url not found")
+		http.Redirect(w, r, "/error", http.StatusNotFound)
+		// handleError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, url, http.StatusSeeOther)
+}
+
+func ErrorHandler(w http.ResponseWriter, r *http.Request) {
+	templates.RenderTemplate(w, "error", models.Url{})
 }
 
 func ViewHandler(w http.ResponseWriter, r *http.Request) {
-	// url := r.FormValue("url")
 
 	// var shortUrl string;
 	// if url == "" {
@@ -26,5 +60,27 @@ func ViewHandler(w http.ResponseWriter, r *http.Request) {
 	// 	OriginalUrl: url,
 	// }
 
-	Templates.RenderTemplate(w, "view", Models.Url{})
+	templates.RenderTemplate(w, "view", models.Url{})
+}
+
+func handleError(w http.ResponseWriter, err error) {
+	switch err {
+	case custom_errors.ErrNotFound, sql.ErrNoRows:
+		http.Error(w, "Not Found", http.StatusNotFound)
+
+	case custom_errors.ErrBadRequest:
+		w.WriteHeader(http.StatusOK)
+		// http.Error(w, "Bad Request", http.StatusBadRequest)
+
+	case custom_errors.ErrUnauthorized:
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+
+	default:
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+func isInt(value *string) bool {
+	_, err := strconv.Atoi(*value)
+	return err == nil
 }
